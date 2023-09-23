5adeca993c596de806631f797ed09900c6cc95b8
Taylor Girard <686taytay@gmail.com>
2023-09-22 21:45:56 -0700

    fixed typo on home page

--- cc-user-interface/src/pages/home.js
+++ cc-user-interface/src/pages/home.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 function Home() {
   return (
-    <div className="SignUpPage">
+    <div className="HomePage">
       <div className="WelcomeHeader">
         <Link to="/signup"><button class="signUpButton">Sign Up</button></Link>
       </div>
