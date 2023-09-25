e2f6af59f0dfb7c87a30261bcceffdc26ea41dde
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2023-09-25 15:01:47 -0700

    create Authentication controller

---
+++ cc-api/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using cc_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace cc_api.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly CozyCocktailsContext _context;
+
+        public AuthenticationController(CozyCocktailsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserDto credentials)
+        {
+           
+            return Ok("User registered successfully");
+        }
+
+        public class UserDto
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, MinimumLength = 6)]
+            public string Password { get; set; }
+        }
+    }
+}
