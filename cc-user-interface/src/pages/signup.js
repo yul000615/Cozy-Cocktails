db8fa8c9632999cd88e7151f6dd9d6997c4f14aa
Taylor Girard <686taytay@gmail.com>
2023-09-28 16:49:10 -0700

    added simple sign up confirmation pop-up

--- cc-user-interface/src/pages/signup.js
+++ cc-user-interface/src/pages/signup.js
@@ -1,5 +1,7 @@
 import './signup.css'
 import {useState} from 'react'
+import Modal from 'react-modal'
+import {Link} from 'react-router-dom'
 
 function ErrorMessages(error){
   if (error.length === 0){
@@ -16,7 +18,8 @@ function SignUp() {
     const [last, setLast] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [existingEmail, setExistingEmail] = useState(true)
+    const [existingEmail, setExistingEmail] = useState(false)
+    const [modalOpen, setModalOpen] = useState(false)
 
     function registerClick() {
 
@@ -26,16 +29,23 @@ function SignUp() {
       if (first.length === 0 || last.length === 0 || email.length === 0 || password.length === 0){
         setError('All values must be filled');
       } else if (password.length >= 64){
-        setError('Password must be less than 64 characters')
+        setError('Password must be less than 64 characters');
       } else if (!hasSpecial.test(password) || !hasNumber.test(password)){
-        setError('Password must contain special character and number')
+        setError('Password must contain special character and number');
       } else if (existingEmail){
-        setError('Email already is linked to an account')
+        setError('Email already is linked to an account');
       } else {
-        setError('')
+        setError('');
+        setModalOpen(true);
       }
 
     }
+
+    function closeModal() {
+      setModalOpen(false);
+    }
+
+
     return (
       <div className="SignUpPage">
         <div className="SignUpWelcome">
@@ -61,6 +71,11 @@ function SignUp() {
             </label>
             <br/>
             <button className='registerBtn' onClick={registerClick}>Register</button>
+            <Modal isOpen={modalOpen} onRequestClose={closeModal} className="Modal">
+              <h2>Account Creation Successful!</h2>
+              <br/>
+              <Link to="/"><button onClick={closeModal}>Head to home page</button></Link>
+            </Modal>
         </div>
       </div>
     );
