d96010d3e176ed32976334ff3f84d684e1cb8aba
Ryan Gieg <47954352+Malarkey327@users.noreply.github.com>
2023-10-01 15:06:58 -0700

    Created Unit of Work class for dbcontext consistency, Generic Repository for CRUD methods, and model specific implementations of the generic repository.

---
+++ cc-api/DAL/RecipeRepository.cs
@@ -0,0 +1,11 @@
+﻿using cc_api.Models;
+
+namespace cc_api.DAL
+{
+    public class RecipeRepository : GenericRepository<Recipe>
+    {
+        public RecipeRepository(CozyCocktailsContext context) : base(context)
+        {
+        }
+    }
+}
