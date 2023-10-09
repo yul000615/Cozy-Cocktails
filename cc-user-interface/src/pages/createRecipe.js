921f8e8c3ca797b9221bc9e0c423903bf8355a42
Taylor Girard <686taytay@gmail.com>
2023-10-08 23:53:31 -0700

    added data storage in formData upon recipe submit

--- cc-user-interface/src/pages/createRecipe.js
+++ cc-user-interface/src/pages/createRecipe.js
@@ -3,8 +3,10 @@ import {useState} from 'react'
 import Select from "react-select"
 import Modal from 'react-modal'
 import {Link} from 'react-router-dom'
+import { useRef, createRef } from 'react';
 
 export default function CreateRecipe() {
+
     const optionList = [
         { value: "gin", label: "Gin" },
         { value: "rum", label: "Rum" },
@@ -16,16 +18,20 @@ export default function CreateRecipe() {
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     const [selectedOptions, setSelectedOptions] = useState([]);
-    const [modalOpen, setModalOpen] = useState(false)
+    const [modalOpen, setModalOpen] = useState(false);
 
-
-    function submitClick(){
+    function submitClick(e){
         setModalOpen(true);
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        console.log(...formData)
     }
 
     function closeModal() {
         setModalOpen(false);
     }
+
 
     function handleSelect(data) {
         setSelectedOptions(data);
@@ -35,34 +41,36 @@ export default function CreateRecipe() {
     return(
         <div className="CreateRecipeFields">
             <h1>Create your own recipe!</h1>
-            <label className = "entryField">
-            Recipe Name: <input name="name" value={name} onChange={(e) => setName(e.target.value)}/>
-            </label>
-            <br/>
-            <label className = "entryFieldLong">
-            Recipe Description: <textArea className="descriptionField" name="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
-            </label>
-            <div className="dropdown-container">
-                <Select
-                    options={optionList}
-                    value={selectedOptions}
-                    onChange={handleSelect}
-                    isSearchable={true}
-                    isMulti
-                    placeholder="Select ingredients"
-                />
-            </div>
-            {selectedOptions.map((ingredient) => 
-                <div className='quantityField'>
-                    <label className='quantityLabel'>
-                        {ingredient['label']} Amount: <input />
-                    </label>
-                    <label className='quantityLabel'>
-                        Quantity (tbs, ml, etc): <input />
-                    </label>
+            <form onSubmit={submitClick} class="recipeForm">
+                <label className = "entryField">
+                Recipe Name: <input name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+                </label>
+                <br/>
+                <label className = "entryFieldLong">
+                Recipe Description: <textArea className="descriptionField" name="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
+                </label>
+                <div className="dropdown-container">
+                    <Select
+                        options={optionList}
+                        value={selectedOptions}
+                        onChange={handleSelect}
+                        isSearchable={true}
+                        isMulti
+                        placeholder="Select ingredients"
+                    />
                 </div>
-            )}
-            <button className='submitBtn' onClick={submitClick}>Submit</button>
+                {selectedOptions.map((ingredient, i) => 
+                    <div className='quantityField'>
+                        <label className='quantityLabel'>
+                            {ingredient['label']} Amount: <input name={ingredient['value'] + 'Amount'}/>
+                        </label>
+                        <label className='quantityLabel'>
+                            Quantity (tbs, ml, etc): <input name={ingredient['value'] + 'Quantity'}/>
+                        </label>
+                    </div>
+                )}
+                <button type='submit' className='submitBtn'>Submit</button>
+            </form>
             <Modal isOpen={modalOpen} onRequestClose={closeModal} className="Modal">
               <h2>Recipe Successfully Created!</h2>
               <br/>
