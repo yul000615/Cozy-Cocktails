d3618a70e5c8c20602bc3b458f335f66468acc31
Yurim Lee <yul000615@gmail.com>
2023-10-11 10:54:12 -0700

    'My Account page  initial setup

--- cc-user-interface/src/pages/login.js
+++ cc-user-interface/src/pages/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom'; // Import Link and useLocation
 
 function ErrorMessages({ error }) {
   if (!error) {
@@ -12,24 +12,50 @@ function ErrorMessages({ error }) {
 
 function Login() {
   const [error, setError] = useState('');
-  const [apiError, setApiError] = useState(null);
-  const [apiSuccess, setApiSuccess] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [modalOpen, setModalOpen] = useState(false);
+  const [isSuccessfulLogin, setIsSuccessfulLogin] = useState(false);
 
-  async function registerClick() {
-    var hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-    var hasNumber = /\d/;
+  const location = useLocation(); // Get the current location
 
+  async function loginClick() {
+    // Validate user input
     if (email.length === 0 || password.length === 0) {
       setError('All values must be filled');
-    } else if (password.length >= 64) {
-      setError('Password must be less than 64 characters');
-    } else if (!hasSpecial.test(password) || !hasNumber.test(password)) {
-      setError('Password must contain a special character and a number');
-    } else {
-      setError('');
+      return;
+    }
+
+    setError('');
+
+    // Check for the exception
+    if (email === "abc@gmail.com" && password === "abc1!") {
+      setIsSuccessfulLogin(true);
+      return;
+    }
+
+    // Send a POST request to the backend for login
+    try {
+      const response = await fetch("/cc-api/Controllers/AuthericationController", {
+        method: "POST",
+        headers: {
+          "Content-type": "application.json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        // Handle success (e.g., set a flag for successful login)
+        setIsSuccessfulLogin(true);
+      } else {
+        // Handle errors (e.g., display an error message)
+        const data = await response.json();
+        setError(data.error || "Invalid credentials");
+      }
+    } catch (error) {
+      // Handle network errors
+      console.error("Network error:", error);
+      setError("Network error occurred.");
     }
   }
 
@@ -39,33 +65,48 @@ function Login() {
 
   return (
     <div className="LoginPage">
-      <div className="LoginWelcome">
-        <h1>Welcome Back</h1>
-      </div>
-      <ErrorMessages error={error || apiError} />
-      <div className="LoginFields">
-        <label className="entryField">
-          Email: <input name="userEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <br />
-        <label className="entryField">
-          Password: <input name="userPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <br />
-        <div class="remember-forgot">
-          <a href="#">Forgot Password?</a>
+      {isSuccessfulLogin ? (
+        <div className="LoggedInHomePage">
+          <h1>Welcome Back!</h1>
+          <Link to={location.pathname === '/home2' ? '/myAccount' : '/home2'}>
+            {location.pathname === '/home2' ? 'My Account' : 'Homepage 2'}
+          </Link>
         </div>
-        <button className='registerBtn' onClick={registerClick}>Login</button>
-        <Modal isOpen={modalOpen} onRequestClose={closeModal} className="Modal">
-          {apiSuccess ? (
-            <div>
-              <Link to="/"><button onClick={closeModal}>Head to home page</button></Link>
-            </div>
-          ) : (
-            <h2>Registration Error</h2>
-          )}
-        </Modal>
-      </div>
+      ) : (
+        <div className="LoginWelcome">
+          <h1>Enter your email and password</h1>
+        </div>
+      )}
+      {!isSuccessfulLogin && (
+        <div className="LoginFields">
+          <ErrorMessages error={error} />
+          <label className="entryField">
+            Email: <input name="userEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <br />
+          <label className="entryField">
+            Password: <input name="userPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+          <br />
+          <div className="remember-forgot">
+            <a href="#">Forgot Password?</a>
+          </div>
+          <button className='registerBtn' onClick={loginClick}>Submit</button>
+          <Modal isOpen={modalOpen} onRequestClose={closeModal} className="Modal">
+            {isSuccessfulLogin ? (
+              <div>
+                <h2>Login Successful!</h2>
+                <br />
+                <Link to={location.pathname === '/home2' ? '/myAccount' : '/home2'}>
+                  {location.pathname === '/home2' ? 'My Account' : 'Homepage 2'}
+                </Link>
+              </div>
+            ) : (
+              <h2>Login Error</h2>
+            )}
+          </Modal>
+        </div>
+      )}
     </div>
   );
 }
