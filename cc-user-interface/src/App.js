29a785aa5da899b84920357c7c601f2ab696d86e
Yurim Lee <yul000615@gmail.com>
2023-10-11 10:48:07 -0700

    2nd Homepage that user accesses after successfully login

--- cc-user-interface/src/App.js
+++ cc-user-interface/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home';
+import Home2 from './pages/home2';
 import SignUp from './pages/signup';
 import Login from './pages/login';
 import CreateRecipe from './pages/createRecipe';
@@ -12,6 +13,7 @@ function App() {
     <Router>
         <Routes>
             <Route exact path='/' element={<Home />} />
+            <Route exact path='/home2' element={<Home2 />} />
             <Route exact path='/login' element={<Login />} />
             <Route exact path='/signup' element={<SignUp />} />
             <Route exact path='/createRecipe' element={<CreateRecipe />} /> 
