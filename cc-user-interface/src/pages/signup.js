21b8957916a7ab80fc0db3cfe18beccc0313d1f2
Yurim Lee <yul000615@gmail.com>
2023-10-19 11:33:31 -0700

    Fail to fetch error troubleshoot

--- cc-user-interface/src/pages/signup.js
+++ cc-user-interface/src/pages/signup.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
-import { Link } from 'react-router-dom';
-import './signup.css'
+const React = require('react');
+const { useState } = require('react');
+const Modal = require('react-modal');
+const { Link } = require('react-router-dom');
+require('./signup.css');
 
 Modal.setAppElement('#root');
 
@@ -13,7 +14,7 @@ function ErrorMessages({ error }) {
   }
 }
 
-export const SignUp = (props) =>{
+export const SignUp = (props) => {
   const [error, setError] = useState('');
   const [apiError, setApiError] = useState(null);
   const [apiSuccess, setApiSuccess] = useState(false);
@@ -25,57 +26,58 @@ export const SignUp = (props) =>{
   const [existingEmail, setExistingEmail] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
 
-
-  const formData = {
-    email,
-    password,
-  };
-
   const registerClick = async (e) => {
     e.preventDefault();
     console.log("Email: ", email);
     console.log("Password: ", password);
 
-    var hasAt = /@/;
-    var hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-    var hasNumber = /\d/;
+    var emailValidation = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    var passwordValidation = /^(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*\d).*$/;
 
     if (first.length === 0 || last.length === 0 || email.length === 0 || password.length === 0) {
       setError('All values must be filled');
-    } else if (!hasAt.test(email)) {
+    } else if (!emailValidation.test(email)) {
       setError('Enter a proper email');
-    } else if (password.length >= 64) {
+    } else if (password.length > 64) {
       setError('Password must be less than 64 characters');
-    } else if (!hasSpecial.test(password) || !hasNumber.test(password)) {
+    } else if (!passwordValidation.test(password)) {
       setError('Password must contain a special character and a number');
     } else if (existingEmail) {
       setError('Email is already linked to an account');
     } else {
       setError('');
 
+
       try {
-        const response = await fetch("/api/Account/register", {
+        const response = await fetch("http://localhost:7268/api/Account/register", {
           method: "POST",
           headers: {
-            "Content-type": "application/json",
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify(formData),
+          body: JSON.stringify({
+            FirstName: first,
+            LastName: last,
+            Email: email,
+            Password: password,
+          }),
         });
-
-        if (response.ok) {
-          // Handle success
-          setApiSuccess(true);
-          setModalOpen(true);
-        } else {
-          // Handle errors
-          const data = await response.json();
-          setApiError(data.error || "An error occurred during registration.");
+  
+        if (!response.ok) {
+          const errorMessage = `HTTP error! Status: ${response.status}`;
+          const errorResponse = await response.text(); 
+          console.error(errorMessage, errorResponse); 
+          throw new Error(errorMessage);
         }
+  
+        const data = await response.json();
+        setApiSuccess(true);
+        setModalOpen(true);
       } catch (error) {
-        console.error("Network error:", error);
-        setApiError("Network error occurred.");
+        console.error('Error during fetch:', error);
+        setApiError("A network error occurred.");
+        setModalOpen(true);
       }
-    }
+    };
   }
 
   function closeModal() {
@@ -92,19 +94,19 @@ export const SignUp = (props) =>{
       <ErrorMessages error={error || apiError} />
       <div className="SignUpFields">
         <label htmlFor="name" className="entryField"></label>
-          First Name: <input name="first" value={first} onChange={(e) => setFirst(e.target.value)} 
+          First Name: <input name="firstName" value={first} onChange={(e) => setFirst(e.target.value)}
           type="first" id="first" />
         <br />
         <label htmlFor="name" className="entryField"></label>
-          Last Name: <input name="last" value={last} onChange={(e) => setLast(e.target.value)} 
+          Last Name: <input name="lastName" value={last} onChange={(e) => setLast(e.target.value)}
           type="last" id="last" />
         <br />
         <label htmlFor="email" className="entryField"></label>
-          Email: <input name="email" onChange={(e) => setEmail(e.target.value)} 
+          Email: <input name="email" onChange={(e) => setEmail(e.target.value)}
           type="email" id="email" />
         <br />
         <label htmlFor="password" className="entryField"></label>
-          Password: <input name="password" value={password} onChange={(e) => setPassword(e.target.value)} 
+          Password: <input name="password" value={password} onChange={(e) => setPassword(e.target.value)}
           type="password" id="password" />
         <br />
         <button className='registerBtn' onClick={registerClick}>Register</button>
