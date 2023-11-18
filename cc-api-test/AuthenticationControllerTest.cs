79fda0ee1563575bfbe3b155c5b83588a58de7e7
Ryan Gieg <47954352+Malarkey327@users.noreply.github.com>
2023-11-18 14:06:01 -0800

    Db migration to add ABV and average rating to recipe table

--- cc-api-test/AuthenticationControllerTest.cs
+++ cc-api-test/AuthenticationControllerTest.cs
@@ -30,6 +30,7 @@ namespace cc_api_test
         private readonly Mock<RefreshTokenValidator> _refreshValidateMock;
         private readonly Mock<AuthenticationConfiguration> _configMock;
         private readonly Mock<Authenticator> _authenticateMock;
+        private readonly Mock<TokenReader> _tokenReaderMock;
         #endregion
 
         #region Constructors
@@ -61,8 +62,9 @@ namespace cc_api_test
             _refreshValidateMock = new Mock<RefreshTokenValidator>(_configMock.Object);
             _authenticateMock = new Mock<Authenticator>(_uowMock.Object, _accessTokenMock.Object, _refreshTokenMock.Object);
 
+            _tokenReaderMock = new Mock<TokenReader>();
 
-            _controller = new AuthenticationController(_uowMock.Object, _hashMock.Object, _authenticateMock.Object, _refreshValidateMock.Object);
+            _controller = new AuthenticationController(_uowMock.Object, _hashMock.Object, _authenticateMock.Object, _refreshValidateMock.Object, _tokenReaderMock.Object);
         }
         #endregion
 
